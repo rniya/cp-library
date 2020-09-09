d82552053557c2cb87b6fa36656f34a5acc83208
rniya <keik5x17@gmail.com>
2020-09-09 13:59:49 +0900

    add

---
+++ datastructure/UnionFind.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "../base.hpp"
+
+struct UnionFind{
+    int num;
+    vector<int> par,rank;
+    UnionFind(int n):num(n),par(n),rank(n,1){
+        iota(par.begin(),par.end(),0);
+    }
+    int root(int x){
+        return (par[x]==x?x:par[x]=root(par[x]));
+    }
+    bool merge(int x, int y){
+        x=root(x); y=root(y);
+        if (x==y) return false;
+        if (rank[x]<rank[y]) swap(x,y);
+        par[y]=x;
+        rank[x]+=rank[y];
+        --num;
+        return true;
+    }
+    bool same(int x, int y){return root(x)==root(y);}
+    int size(int x){return rank[root(x)];}
+    int count(){return num;}
+};
