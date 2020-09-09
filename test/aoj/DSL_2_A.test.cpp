7055f5c9ddad30664fddd9cfdb5c1ac2db72ccec
rniya <keik5x17@gmail.com>
2020-09-09 16:06:27 +0900

    update

---
+++ test/aoj/DSL_2_A.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/all/DSL_2_A"
+
+#include "../../base.hpp"
+#include "../../datastructure/SegmentTree.hpp"
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int n,q; cin >> n >> q;
+
+    SegmentTree<int> seg(n,[](int a,int b){return min(a,b);},INT_MAX);
+
+    for (;q--;){
+        int c,x,y; cin >> c >> x >> y;
+        if (!c) seg.update(x,y);
+        else cout << seg.query(x,y+1) << '\n';
+    }
+}
