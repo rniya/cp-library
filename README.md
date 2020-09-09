a85a676456bfadffbdf564c0e9e5eee32443dfcf
rniya <keik5x17@gmail.com>
2020-09-09 15:10:28 +0900

    update

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 competitive programming library (C++ 14)
 
 [![Actions Status](https://github.com/rniya/library/workflows/verify/badge.svg)](https://github.com/rniya/library/actions)
-
  [![GitHub Pages](https://img.shields.io/static/v1?label=GitHub+Pages&message=+&color=brightgreen&logo=github)](https://rniya.github.io/library/)
---
+++ test/aoj/DSL_1_A.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/1/DSL_1_A"
+
+#include "../../base.hpp"
+#include "../../datastructure/UnionFind.hpp"
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int n,q; cin >> n >> q;
+
+    UnionFind UF(n);
+
+    for (;q--;){
+        int c,x,y; cin >> c >> x >> y;
+        if (!c) UF.merge(x,y);
+        else cout << UF.same(x,y) << '\n';
+    }
+}
