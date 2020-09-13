1c77e95fa315236ac91a2387bf8fc480aaa0acf0
rniya <keik5x17@gmail.com>
2020-09-13 15:43:48 +0900

    add

---
+++ convolution/ArbitaryModConvolution.hpp
@@ -0,0 +1,41 @@
+/**
+ * @brief Arbirary Mod Convolution (任意mod畳み込み)
+ * @docs docs/convolution/ArbitaryModConvolution.md
+ */
+
+#pragma once
+
+#include "../base.hpp"
+#include "../modulo/modint.hpp"
+#include "../convolution/NumberTheoreticTransform.hpp"
+
+template<typename M>
+vector<M> ArbitaryModConvolution(const vector<M> &a,const vector<M> &b){
+    int n=a.size(),m=b.size();
+    static constexpr int mod0=167772161,mod1=469762049,mod2=1224736769;
+    using mint0=modint<mod0>;
+    using mint1=modint<mod1>;
+    using mint2=modint<mod2>;
+    NumberTheoreticTransform<mod0> ntt0;
+    NumberTheoreticTransform<mod1> ntt1;
+    NumberTheoreticTransform<mod2> ntt2;
+    vector<mint0> a0(n),b0(m);
+    vector<mint1> a1(n),b1(m);
+    vector<mint2> a2(n),b2(m);
+    for (int i=0;i<n;++i) a0[i]=a[i].a,a1[i]=a[i].a,a2[i]=a[i].a;
+    for (int i=0;i<m;++i) b0[i]=b[i].a,b1[i]=b[i].a,b2[i]=b[i].a;
+    auto c0=ntt0.multiply(a0,b0);
+    auto c1=ntt1.multiply(a1,b1);
+    auto c2=ntt2.multiply(a2,b2);
+    static const mint1 inv0=(mint1)1/mod0;
+    static const mint2 inv1=(mint2)1/mod1,inv0inv1=inv1/mod0;
+    static const M m0=mod0,m0m1=m0*mod1;
+    vector<M> res(n+m-1);
+    for (int i=0;i<n+m-1;++i){
+        int v0=c0[i].a;
+        int v1=(inv0*(c1[i]-v0)).a;
+        int v2=(inv0inv1*(c2[i]-v0)-inv1*v1).a;
+        res[i]=v0+m0*v1+m0m1*v2;
+    }
+    return res;
+}
---
+++ test/yosupo/convolution_mod_1000000007.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod_1000000007"
+
+#include "../../base.hpp"
+#include "../../convolution/ArbitaryModConvolution.hpp"
+
+using mint=modint<1000000007>;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int N,M; cin >> N >> M;
+    vector<mint> a(N),b(M);
+    for (int i=0;i<N;++i) cin >> a[i];
+    for (int i=0;i<M;++i) cin >> b[i];
+
+    vector<mint> c=ArbitaryModConvolution(a,b);
+    for (int i=0;i<N+M-1;++i) cout << c[i] << (i==N+M-2?'\n':' ');
+}
