fd4ca72d19644b680b174f7156c63be3c1e1df01
rniya <keik5x17@gmail.com>
2020-09-14 16:34:26 +0900

    add

---
+++ test/yukicoder/526.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://yukicoder.me/problems/no/526"
+
+#include "../../base.hpp"
+#include "../../linearalgebra/Matrix.hpp"
+#include "../../modulo/runtime_modint.hpp"
+
+using mint=runtime_modint;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int N,M; cin >> N >> M;
+    mint::set_mod(M);
+    Matrix<mint> m(2);
+    m[0][1]=m[1][0]=m[1][1]=1;
+    m^=N;
+    cout << m[0][0] << '\n';
+}
