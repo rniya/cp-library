e853608bc9d4b3830ab4b8e9ae5256951d494cb4
rniya <keik5x17@gmail.com>
2020-09-15 13:26:33 +0900

    add

--- other/Mo.hpp
+++ other/Mo.hpp
@@ -9,31 +9,31 @@
 
 struct Mo{
     int sz;
-    vector<int> left,right,order;
-    vector<bool> check;
-    Mo(int n,int q):sz((int)sqrt(n)),order(q),check(n){}
+    vector<int> left,right;
+    Mo(int n):sz((int)sqrt(n)){}
     void insert(int l,int r){
         left.emplace_back(l);
         right.emplace_back(r);
     }
-    template<typename ADD,typename DEL,typename REM>
-    void build(const ADD &add,const DEL &del,const REM &rem){
-        iota(order.begin(),order.end(),0);
-        sort(order.begin(),order.end(),[&](int a,int b){
+    template<typename ADDL,typename ADDR,typename DELL,typename DELR,typename REM>
+    void build(const ADDL &add_left,const ADDR &add_right,const DELL &del_left,const DELR &del_right,const REM &rem){
+        int q=left.size();
+        vector<int> ord(q);
+        iota(ord.begin(),ord.end(),0);
+        sort(ord.begin(),ord.end(),[&](int a,int b){
             return (left[a]/sz!=left[b]/sz?left[a]<left[b]:right[a]<right[b]);
         });
         int l=0,r=0;
-        auto push=[&](int idx){
-            check[idx].flip();
-            if (check[idx]) add(idx);
-            else del(idx);
-        };
-        for (int idx:order){
-            while(l>left[idx]) push(--l);
-            while(r<right[idx]) push(r++);
-            while(l<left[idx]) push(l++);
-            while(r>right[idx]) push(--r);
+        for (int idx:ord){
+            while(l>left[idx]) add_left(--l);
+            while(r<right[idx]) add_right(r++);
+            while(l<left[idx]) del_left(l++);
+            while(r>right[idx]) del_right(--r);
             rem(idx);
         }
+    }
+    template<typename ADD,typename DEL,typename REM>
+    void build(const ADD &add,const DEL &del,const REM &rem){
+        build(add,add,del,del,rem);
     }
 };
---
+++ test/yosupo/static_range_inversions_query.test.cpp
@@ -0,0 +1,50 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_inversions_query"
+
+#include "../../base.hpp"
+#include "../../util/compress.hpp"
+#include "../../datastructure/BinaryIndexedTree.hpp"
+#include "../../other/Mo.hpp"
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int N,Q; cin >> N >> Q;
+    vector<int> A(N);
+    for (int i=0;i<N;++i) cin >> A[i];
+
+    Mo mo(N);
+    for (int i=0;i<Q;++i){
+        int l,r; cin >> l >> r;
+        mo.insert(l,r);
+    }
+
+    map<int,int> mp=dict(compress(A));
+    for (int i=0;i<N;++i) A[i]=mp[A[i]];
+    int n=mp.size();
+    BinaryIndexedTree<int> BIT(n+1);
+    vector<long long> ans(Q);
+    long long inv=0; int sum=0;
+    auto add_left=[&](int idx){
+        inv+=BIT.sum0(A[idx]-1);
+        ++sum; BIT.add0(A[idx],1);
+    };
+    auto add_right=[&](int idx){
+        inv+=sum-BIT.sum0(A[idx]);
+        ++sum; BIT.add0(A[idx],1);
+    };
+    auto del_left=[&](int idx){
+        inv-=BIT.sum0(A[idx]-1);
+        --sum; BIT.add0(A[idx],-1);
+    };
+    auto del_right=[&](int idx){
+        inv-=sum-BIT.sum0(A[idx]);
+        --sum; BIT.add0(A[idx],-1);
+    };
+    auto rem=[&](int idx){
+        ans[idx]=inv;
+    };
+
+    mo.build(add_left,add_right,del_left,del_right,rem);
+
+    for (int i=0;i<Q;++i) cout << ans[i] << '\n';
+}
