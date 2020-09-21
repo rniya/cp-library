54f908877841490c9266927d7a8acd2a52efb809
rniya <keik5x17@gmail.com>
2020-09-22 00:43:37 +0900

    add

---
+++ test/yosupo/inv_of_formal_power_series.test.cpp
@@ -0,0 +1,26 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/inv_of_formal_power_series"
+
+#include "../../base.hpp"
+#include "../../modulo/modint.hpp"
+#include "../../convolution/FastFourierTransform.hpp"
+#include "../../polynomial/FormalPowerSeries.hpp"
+
+using mint=modint<998244353>;
+using FPS=FormalPowerSeries<mint>;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    NumberTheoreticTransform<998244353> NTT;
+    auto mul=[&](const FPS::Poly &a,const FPS::Poly &b){
+        auto res=NTT.multiply(a,b);
+        return FPS::Poly(res.begin(),res.end());
+    };
+    FPS::set_mul(mul);
+
+    int N; cin >> N;
+    FPS a(N);
+    for (int i=0;i<N;++i) cin >> a[i];
+    FPS b=a.inv();
+    for (int i=0;i<N;++i) cout << b[i] << (i+1==N?'\n':' ');
+}
