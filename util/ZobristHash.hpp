9a33dc50b774cb03bba838c336f1a4b815c93d68
rniya <keik5x17@gmail.com>
2020-09-22 22:16:44 +0900

    add

---
+++ util/ZobristHash.hpp
@@ -0,0 +1,15 @@
+/**
+ * @brief Zobrist Hash
+ */
+
+#pragma once
+
+#include "../base.hpp"
+
+vector<uint64_t> ZobristHash(int n){
+    random_device seed_gen;
+    mt19937_64 engine(seed_gen());
+    vector<uint64_t> base(n);
+    for (int i=0;i<n;++i) base[i]=engine();
+    return base;
+}
