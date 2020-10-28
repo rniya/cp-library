b099283260db5f9edc12f16f5b20f002252cee35
rniya <keik5x17@gmail.com>
2020-10-28 20:44:16 +0900

    add AhoCorasick

---
+++ test/yukicoder/430.AhoCorasick.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://yukicoder.me/problems/no/430"
+
+#include "../../base.hpp"
+#include "../../string/AhoCorasick.hpp"
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    string S; int M; cin >> S >> M;
+
+    AhoCorasick<26> AHO([](char c){return c-'A';});
+    for (;M--;){
+        string C; cin >> C;
+        AHO.add(C);
+    }
+    AHO.build();
+
+    cout << AHO.move(S).first << '\n';
+}
