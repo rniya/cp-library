7d2692fea4f7f8f0caff92af5c5edfdd5c85d541
rniya <keik5x17@gmail.com>
2020-12-17 19:09:45 +0900

    improve Hopcroft Karp

--- flow/HopcroftKarp.hpp
+++ flow/HopcroftKarp.hpp
@@ -1,6 +1,6 @@
 /**
  * @brief Hopcroft Karp
- * @docs docs/flow/HopcroftKarp
+ * @docs docs/flow/HopcroftKarp.md
  */
 
 #pragma once
@@ -9,19 +9,19 @@
 
 struct HopcroftKarp{
 private:
-    int n,m,time;
+    int n,m; bool matched;
     vector<vector<int>> G,rG;
-    vector<int> match_l,match_r,level,used;
-    void bfs(){
-        fill(level.begin(),level.end(),-1);
+    vector<int> match_l,match_r,level;
+    void levelize(){
         queue<int> que;
         for (int i=0;i<n;++i){
-            if (match_l[i]==-1){
+            level[i]=-1;
+            if (match_l[i]<0){
                 level[i]=0;
                 que.emplace(i);
             }
         }
-        while(!que.empty()){
+        while (!que.empty()){
             int v=que.front(); que.pop();
             for (int u:G[v]){
                 int w=match_r[u];
@@ -32,39 +32,105 @@ private:
             }
         }
     }
-    bool dfs(int v){
-        used[v]=time;
+    bool find_augumenting_path(int v){
         for (int u:G[v]){
             int w=match_r[u];
-            if (w<0||(used[w]!=time&&level[w]==level[v]+1&&dfs(w))){
-                match_r[u]=v; match_l[v]=u;
+            if (w<0||(level[w]>level[v]&&find_augumenting_path(w))){
+                match_l[v]=u; match_r[u]=v;
                 return true;
             }
         }
         return false;
     }
+    vector<vector<int>> build_residual_graph(){
+        if (!matched) max_matching();
+        const int s=n+m,t=n+m+1;
+        vector<vector<int>> res(n+m+2);
+        for (int i=0;i<n;++i){
+            if (match_l[i]<0){
+                res[s].emplace_back(i);
+            }
+        }
+        for (int i=0;i<n;++i){
+            for (int j:G[i]){
+                if (match_l[i]==j) res[j+n].emplace_back(i);
+                else res[i].emplace_back(j+n);
+            }
+        }
+        return res;
+    }
+    vector<bool> find_residual_path(){
+        auto g=build_residual_graph();
+        queue<int> que;
+        vector<bool> visited(n+m+2);
+        que.emplace(n+m); visited[n+m]=true;
+        while (!que.empty()){
+            int v=que.front(); que.pop();
+            for (int u:g[v]){
+                if (visited[u]) continue;
+                visited[u]=true; que.emplace(u);
+            }
+        }
+        return visited;
+    }
+
 public:
-    HopcroftKarp(int n,int m):n(n),m(m),G(n),rG(m),match_l(n,-1),match_r(m,-1),level(n),used(n){}
+    HopcroftKarp(int n,int m):n(n),m(m),G(n),rG(m),match_l(n,-1),match_r(m,-1),level(n),matched(false){}
     void add_edge(int u,int v){
         G[u].emplace_back(v);
         rG[v].emplace_back(u);
     }
     vector<pair<int,int>> max_matching(){
-        while(1){
-            bfs(); ++time;
-            int f=0;
+        matched=true;
+        while (1){
+            levelize();
+            int match=0;
             for (int i=0;i<n;++i){
                 if (match_l[i]<0){
-                    f+=dfs(i);
+                    match+=find_augumenting_path(i);
                 }
             }
-            if (!f) break;
+            if (!match) break;
         }
         vector<pair<int,int>> res;
         for (int i=0;i<n;++i){
-            if (match_l[i]>=0){
+            if (~match_l[i]){
                 res.emplace_back(i,match_l[i]);
             }
+        }
+        return res;
+    }
+    vector<int> min_vertex_cover(){
+        auto visited=find_residual_path();
+        vector<int> res;
+        for (int i=0;i<n+m;++i){
+            if (visited[i]^(i<n)){
+                res.emplace_back(i);
+            }
+        }
+        return res;
+    }
+    vector<int> max_independent_set(){
+        auto visited=find_residual_path();
+        vector<int> res;
+        for (int i=0;i<n+m;++i){
+            if (visited[i]^(i>=n)){
+                res.emplace_back(i);
+            }
+        }
+        return res;
+    }
+    vector<pair<int,int>> min_edge_cover(){
+        auto res=max_matching();
+        for (int i=0;i<n;++i){
+            if (~match_l[i]) continue;
+            if (G[i].empty()) return {};
+            res.emplace_back(i,G[i][0]);
+        }
+        for (int i=0;i<m;++i){
+            if (~match_r[i]) continue;
+            if (rG[i].empty()) return {};
+            res.emplace_back(rG[i][0],i);
         }
         return res;
     }
---
+++ test/yosupo/bipartitematching.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/bipartitematching"
+
+#include "../../base.hpp"
+#include "../../flow/HopcroftKarp.hpp"
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int L,R,M; cin >> L >> R >> M;
+
+    HopcroftKarp HK(L,R);
+    for (;M--;){
+        int a,b; cin >> a >> b;
+        HK.add_edge(a,b);
+    }
+
+    auto ans=HK.max_matching();
+    cout << ans.size() << '\n';
+    for (auto p:ans) cout << p.first << ' ' << p.second << '\n';
+}
