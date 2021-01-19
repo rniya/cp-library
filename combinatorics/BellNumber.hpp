a74de2d57d9f5fd1f69f3116dbd40af6ad577686
rniya <keik5x17@gmail.com>
2021-01-19 13:29:09 +0900

    update:

--- combinatorics/BellNumber.hpp
+++ combinatorics/BellNumber.hpp
@@ -1,26 +1,26 @@
+#pragma once
+#include "../base.hpp"
+
 /**
  * @brief ベル数
  * @docs docs/combinatorics/BellNumber.md
  */
 
-#pragma once
-
-#include "../base.hpp"
-
-template<typename T>
-T bell_number(int n,int k){
-    if (n==0) return 1;
-    k=min(k,n);
+template <typename T> T bell_number(int n, int k) {
+    if (n == 0) return 1;
+    k = min(k, n);
     Combination<T> COM(k);
-    vector<T> sum(k+1);
-    sum[0]=1;
-    for (int i=1;i<=k;++i){
-        if (i&1) sum[i]=sum[i-1]-COM.finv(i);
-        else sum[i]=sum[i-1]+COM.finv(i);
+    vector<T> sum(k + 1);
+    sum[0] = 1;
+    for (int i = 1; i <= k; i++) {
+        if (i & 1)
+            sum[i] = sum[i - 1] - COM.finv(i);
+        else
+            sum[i] = sum[i - 1] + COM.finv(i);
     }
-    T res=0;
-    for (int i=1;i<=k;++i){
-        res+=T(i).pow(n)*COM.finv(i)*sum[k-i];
+    T res = 0;
+    for (int i = 1; i <= k; i++) {
+        res += T(i).pow(n) * COM.finv(i) * sum[k - i];
     }
     return res;
 }
--- combinatorics/PartitionTable.hpp
+++ combinatorics/PartitionTable.hpp
@@ -1,19 +1,17 @@
+#pragma once
+#include "../base.hpp"
+
 /**
  * @brief 分割数テーブル
  * @docs docs/combinatorics/PartitionTable.md
  */
 
-#pragma once
-
-#include "../base.hpp"
-
-template<typename T>
-vector<vector<T>> partition_table(int n,int k){
-    vector<vector<T>> dp(n+1,vector<T>(k+1));
-    dp[0][0]=1;
-    for (int i=0;i<=n;++i){
-        for (int j=1;j<=k;++j){
-            dp[i][j]=dp[i][j-1]+(i-j>=0?dp[i-j][j]:0);
+template <typename T> vector<vector<T>> partition_table(int n, int k) {
+    vector<vector<T>> dp(n + 1, vector<T>(k + 1));
+    dp[0][0] = 1;
+    for (int i = 0; i <= n; i++) {
+        for (int j = 1; j <= k; j++) {
+            dp[i][j] = dp[i][j - 1] + (i - j >= 0 ? dp[i - j][j] : 0);
         }
     }
     return dp;
