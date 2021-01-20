555f57c299ce027f404d887cddce12cc802f529c
rniya <keik5x17@gmail.com>
2021-01-20 11:57:30 +0900

    fix

--- test/aoj/DPL_5_G.test.cpp
+++ test/aoj/DPL_5_G.test.cpp
@@ -1,8 +1,9 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/7/DPL/5/DPL_5_G"
 
 #include "../../base.hpp"
-#include "../../combinatorics/BellNumber.hpp"
 #include "../../combinatorics/combination.hpp"
+
+#include "../../combinatorics/BellNumber.hpp"
 #include "../../modulo/modint.hpp"
 
 using mint = modint<1000000007>;
