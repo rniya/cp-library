663d5dfed138458adb7bf365cc892958c0c9eff8
rniya <keik5x17@gmail.com>
2021-04-01 10:39:32 +0900

    add

---
+++ datastructure/RectangleSet.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include "../base.hpp"
+
+/**
+ * @brief Rectangle Set
+ * @docs docs/datastructure/RectangleSet.md
+ */
+template <typename T> struct RectangleSet {
+    constexpr T inf = numeric_limits<T>::max() / 2;
+    set<pair<T, T>> s;
+    multiset<T> val;
+    RectangleSet() {
+        s.emplace(-1, inf);
+        s.emplace(0, 0);
+        val.emplace(0);
+    }
+
+    void add(T x, T y) {
+        auto itr = s.lower_bound(make_pair(x, y));
+        bool flag = false;
+        T nx = inf, ny = inf;
+        while (1) {
+            itr = prev(itr);
+            if (y < itr->second) break;
+            flag = true;
+            nx = min(nx, itr->first);
+            ny = min(ny, itr->second);
+            val.erase(val.find(itr->first + itr->second));
+            itr = s.erase(itr);
+        }
+        if (flag) {
+            s.emplace(nx, y);
+            val.emplace(nx + y);
+            s.emplace(x, ny);
+            val.emplace(x + ny);
+        }
+    }
+    T query() { return *val.begin(); }
+};
