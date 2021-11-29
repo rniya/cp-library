b709192e635508e554462fb094d1f60e09a9428c
rniya <keik5x17@gmail.com>
2021-11-29 20:12:26 +0900

    update

--- test/yosupo/exp_of_formal_power_series.test.cpp
+++ test/yosupo/exp_of_formal_power_series.test.cpp
@@ -1,21 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/exp_of_formal_power_series"
 
-#include <iostream>
 #include "polynomial/FormalPowerSeries.hpp"
-
-namespace atcoder {
-
-template <int MOD> std::istream& operator>>(std::istream& is, static_modint<MOD>& x) {
-    int64_t v;
-    x = static_modint<MOD>{(is >> v, v)};
-    return is;
-}
-
-template <int MOD> std::ostream& operator<<(std::ostream& os, const static_modint<MOD>& x) { return os << x.val(); }
-
-template <int ID> std::ostream& operator<<(std::ostream& os, const dynamic_modint<ID>& x) { return os << x.val(); }
-
-}  // namespace atcoder
+#include "util/modint.hpp"
 
 using mint = atcoder::modint998244353;
 using FPS = FormalPowerSeries<mint>;
---
+++ util/modint.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "atcoder/modint"
+
+namespace atcoder {
+
+template <int MOD> std::istream& operator>>(std::istream& is, static_modint<MOD>& x) {
+    int64_t v;
+    x = static_modint<MOD>{(is >> v, v)};
+    return is;
+}
+
+template <int MOD> std::ostream& operator<<(std::ostream& os, const static_modint<MOD>& x) { return os << x.val(); }
+
+template <int ID> std::ostream& operator<<(std::ostream& os, const dynamic_modint<ID>& x) { return os << x.val(); }
+
+}  // namespace atcoder
