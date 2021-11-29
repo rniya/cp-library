9337b3dd9161ce5fb2e4ea548104bfe5a311ca29
rniya <keik5x17@gmail.com>
2021-11-29 17:18:10 +0900

    test

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 競技プログラミングで使用するライブラリ集です。C++ 14で書かれています。
 
 [![Actions Status](https://github.com/rniya/competitive-programming-library/workflows/verify/badge.svg)](https://github.com/rniya/competitive-programming-library/actions)
- [![GitHub Pages](https://img.shields.io/static/v1?label=GitHub+Pages&message=+&color=brightgreen&logo=github)](https://rniya.github.io/competitive-programming-library/)
+[![GitHub Pages](https://img.shields.io/static/v1?label=GitHub+Pages&message=+&color=brightgreen&logo=github)](https://rniya.github.io/competitive-programming-library/)
+[![rniya](https://img.shields.io/endpoint?url=https%3A%2F%2Fatcoder-badges.now.sh%2Fapi%2Fatcoder%2Fjson%2Frniya)](https://atcoder.jp/users/rniya)
+[![rniya](https://img.shields.io/endpoint?url=https%3A%2F%2Fatcoder-badges.now.sh%2Fapi%2Fcodeforces%2Fjson%2Frniya)](https://codeforces.com/profile/rniya)
--- datastructure/Slide_Min.hpp
+++ datastructure/Slide_Min.hpp
@@ -1,20 +1,22 @@
 #pragma once
-#include "../base.hpp"
+#include <deque>
+#include <vector>
 
-/**
- * @brief スライド最小値
- * @docs docs/datastructure/Slide_Min.md
- */
-template <typename T> vector<T> Slide_Min(const vector<T>& v, int k) {
-    deque<int> deq;
-    vector<T> res;
+template <typename T> std::vector<T> Slide_Min(const std::vector<T>& v, int k) {
+    std::deque<int> deq;
+    std::vector<T> res;
     for (int i = 0; i < (int)v.size(); i++) {
-        while (!deq.empty() && v[deq.back()] >= v[i]) deq.pop_back();
-        deq.push_back(i);
+        while (!deq.empty() and v[deq.back()] >= v[i]) deq.pop_back();
+        deq.emplace_back(i);
         if (i - k + 1 >= 0) {
-            res.push_back(v[deq.front()]);
+            res.emplace_back(v[deq.front()]);
             if (deq.front() == i - k + 1) deq.pop_front();
         }
     }
     return res;
 }
+
+/**
+ * @brief スライド最小値
+ * @docs docs/datastructure/Slide_Min.md
+ */
