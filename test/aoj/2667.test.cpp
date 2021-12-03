0327fd1c370ac2ea21f5196157e2f537f3dc7c96
rniya <keik5x17@gmail.com>
2021-12-04 00:46:31 +0900

    delete some files

--- test/aoj/2667.test.cpp
+++ test/aoj/2667.test.cpp
@@ -1,50 +1,55 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/2667"
 
-#include "../../base.hpp"
-#include "../../datastructure/LazySegmentTree.hpp"
-#include "../../tree/HeavyLightDecomposition.hpp"
+#include "atcoder/lazysegtree"
+#include "tree/HeavyLightDecomposition.hpp"
+
+struct S {
+    long long sum;
+    int cnt;
+    S(long long sum, int cnt) : sum(sum), cnt(cnt) {}
+};
+
+S op(S l, S r) { return S(l.sum + r.sum, l.cnt + r.cnt); }
+
+S e() { return S(0, 0); }
+
+S mapping(int x, S a) { return S(a.sum + x * a.cnt, a.cnt); }
+
+int composition(int l, int r) { return l + r; }
+
+int id() { return 0; }
 
 int main() {
-    cin.tie(0);
-    ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::ios::sync_with_stdio(false);
     int N, Q;
-    cin >> N >> Q;
-
+    std::cin >> N >> Q;
     HeavyLightDecomposition HLD(N);
     for (int i = 0; i < N - 1; i++) {
         int a, b;
-        cin >> a >> b;
+        std::cin >> a >> b;
         HLD.add_edge(a, b);
     }
     HLD.build();
 
-    struct node {
-        long long a;
-        int b;
-        node(long long a, int b) : a(a), b(b) {}
-    };
-    auto f = [](node a, node b) { return node(a.a + b.a, a.b + b.b); };
-    auto g = [](node a, long long x) { return node(a.a + x * a.b, a.b); };
-    auto h = [](long long a, long long b) { return a + b; };
-    LazySegmentTree<node, long long, decltype(f), decltype(g), decltype(h)> seg(N, f, g, h, node(0, 0), 0);
-    for (int i = 0; i < N; i++) seg.set(i, node(0, 1));
-    seg.build();
+    std::vector<S> v(N, S(0, 1));
+    atcoder::lazy_segtree<S, op, e, int, mapping, composition, id> seg(v);
 
     for (; Q--;) {
         int t;
-        cin >> t;
+        std::cin >> t;
         if (!t) {
             int u, v;
-            cin >> u >> v;
+            std::cin >> u >> v;
             long long ans = 0;
-            auto q = [&](int l, int r) { ans += seg.query(l, r).a; };
+            auto q = [&](int l, int r) { ans += seg.prod(l, r).sum; };
             HLD.query_path(u, v, q);
-            cout << ans << '\n';
+            std::cout << ans << '\n';
         } else {
             int v;
             long long x;
-            cin >> v >> x;
-            auto q = [&](int l, int r) { seg.update(l, r, x); };
+            std::cin >> v >> x;
+            auto q = [&](int l, int r) { seg.apply(l, r, x); };
             HLD.query_subtree(v, q);
         }
     }
--- test/yosupo/vertex_add_path_sum.test.cpp
+++ test/yosupo/vertex_add_path_sum.test.cpp
@@ -1,45 +1,48 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/vertex_add_path_sum"
 
-#include "../../base.hpp"
-#include "../../datastructure/SegmentTree.hpp"
-#include "../../tree/HeavyLightDecomposition.hpp"
+#include <iostream>
+#include "atcoder/segtree"
+#include "tree/HeavyLightDecomposition.hpp"
+
+long long op(long long l, long long r) { return l + r; }
+
+long long e() { return 0; }
 
 int main() {
-    cin.tie(0);
-    ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::ios::sync_with_stdio(false);
     int N, Q;
-    cin >> N >> Q;
-    vector<long long> a(N);
-    for (int i = 0; i < N; i++) cin >> a[i];
-
+    std::cin >> N >> Q;
+    std::vector<int> a(N);
+    for (int i = 0; i < N; i++) std::cin >> a[i];
     HeavyLightDecomposition HLD(N);
     for (int i = 0; i < N - 1; i++) {
         int u, v;
-        cin >> u >> v;
+        std::cin >> u >> v;
         HLD.add_edge(u, v);
     }
-    HLD.build();
 
-    auto f = [](long long a, long long b) { return a + b; };
-    SegmentTree<long long, decltype(f)> seg(N, f, 0);
-    for (int i = 0; i < N; i++) seg.set(HLD.idx(i), a[i]);
-    seg.build();
+    HLD.build();
+    std::vector<long long> v(N);
+    for (int i = 0; i < N; i++) v[HLD.idx(i)] = a[i];
+    atcoder::segtree<long long, op, e> seg(v);
 
     for (; Q--;) {
         int t;
-        cin >> t;
+        std::cin >> t;
         if (!t) {
             int p;
             long long x;
-            cin >> p >> x;
-            seg.add(HLD.idx(p), x);
+            std::cin >> p >> x;
+            int idx = HLD.idx(p);
+            seg.set(idx, seg.get(idx) + x);
         } else {
             int u, v;
-            cin >> u >> v;
+            std::cin >> u >> v;
             long long ans = 0;
-            auto q = [&](int l, int r) { ans += seg.query(l, r); };
+            auto q = [&](int l, int r) { ans += seg.prod(l, r); };
             HLD.query_path(u, v, q, true);
-            cout << ans << '\n';
+            std::cout << ans << '\n';
         }
     }
     return 0;
